af5a291e8573f0b5c9fe165e95dcc5aadbdee397
zhirezhixin <yifeil@berkeley.edu>
2020-02-15 12:27:46 -0800

    feat: add sha-256 function

---
+++ back_src/hash.py
@@ -0,0 +1,15 @@
+
+import hashlib
+
+def sha256_hash(str):
+	"""
+		Args:
+			the string to be hashed
+	
+		Return:
+			the hashed hexcode
+
+		Raises:
+			TypeError
+	"""
+	return s = hashlib.sha256(bytes(str, encoding='utf-8')).hexdigest()
