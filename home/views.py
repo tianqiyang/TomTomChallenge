557130063350e213bbe07d5ebd006c67380afe74
Tianqi Yang <tianqiy@berkeley.edu>
2020-05-25 16:47:20 -0700

    show picture in modal

--- home/views.py
+++ home/views.py
@@ -3,55 +3,46 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from django.template import loader
 
-#update data that able to render in html
-def fixRender(args):
-    for cook_type, food_types in args.items():
-        for food_type, foods in food_types.items():
-            for i in range(len(foods)):
-                description = "<p>from prok rice</p>"
-                args[cook_type][food_type][i]['description'] = description
-
 def default_map(request):
     mapbox_access_token = 'pk.my_mapbox_access_token'
     # template = loader.get_template('default.html')
     context = {
         "food":
-        [{
-            "x": -122.41157518699714,
-            "y": 37.78861143786813,
-            "isCook": "cook",
-            "type": "asian",
-            "food_name": "Pork Rice",
-            "Ingredients": "Fried pork, eggs,tomatoes, onions, brown rice",
-            "address": "865 Market St, San Francisco, CA 94103",
-            "food_image": "img/pork_rice.jpg",
-            "restaurant_logo": "img/Panda_Express_logo.png",
-            "other_logos": ["img/1.png", "img/324.png", "img/323.png"],
-            "descirption": "from prok rice"
-        }, {
-            "x": -122.43563039769336,
-            "y": 37.79623397692485,
-            "food_name": "Food_2",
-            "Ingredients": "Fried pork, eggs,tomatoes, onions, brown rice",
-            "address": "865 Market St, San Francisco, CA 94103",
-            "food_image": "img/pork_rice.jpg",
-            "restaurant_logo": "img/Panda_Express_logo.png",
-            "other_logos": ["img/1.png", "img/324.png", "img/323.png"],
-            "descirption": "from prok rice2"
-        }, {
-            "x": -122.45373134831641, 
-            "y": 37.780423390391235,
-            "isCook": "cook",
-            "type": "fast_food",
-            "food_name": "Food_3",
-            "Ingredients": "Fried pork, eggs,tomatoes, onions, brown rice",
-            "address": "865 Market St, San Francisco, CA 94103",
-            "food_image": "img/pork_rice.jpg",
-            "restaurant_logo": "img/Panda_Express_logo.png",
-            "other_logos": ["img/1.png", "img/23.png", "img/323.png"],
-            "descirption": "from prok rice3"
-        }
-    ]
+            [{
+                "x": -122.41157518699714,
+                "y": 37.78861143786813,
+                "isCook": "cook",
+                "type": "asian",
+                "food_name": "Pork Rice",
+                "Ingredients": "Fried pork, eggs,tomatoes, onions, brown rice",
+                "address": "865 Market St, San Francisco, CA 94103",
+                "food_image": "img/pork_rice.jpg",
+                "restaurant_logo": "img/Panda_Express_logo.png",
+                "other_logos": ["img/1.png", "img/324.png", "img/323.png"],
+                "descirption": "<img src=\" static 'img/pork_rice.jpg' %}\""
+            }, {
+                "x": -122.43563039769336,
+                "y": 37.79623397692485,
+                "food_name": "Dragon Liver Noodle",
+                "Ingredients": "Pig liver, noodle",
+                "address": "832 Market St, San Francisco, CA 94103",
+                "food_image": "img/pork_rice.jpg",
+                "restaurant_logo": "img/Panda_Express_logo.png",
+                "other_logos": ["img/1.png", "img/324.png", "img/323.png"],
+                "descirption": "dragon Liver Noodle"
+            }, {
+                "x": -122.45373134831641, 
+                "y": 37.780423390391235,
+                "isCook": "cook",
+                "type": "fast_food",
+                "food_name": "Burritos",
+                "Ingredients": "bread, meat, cheese",
+                "address": "901 Market St, San Francisco, CA 94103",
+                "food_image": "img/beef_cheese_burritos.jpeg",
+                "restaurant_logo": "img/Panda_Express_logo.png",
+                "other_logos": ["img/1.png", "img/23.png", "img/323.png"],
+                "descirption": "Burritos"
+            }
+        ]
     }
-    #return HttpResponse(template.render(context, request))
     return render(request, 'default.html', context)
