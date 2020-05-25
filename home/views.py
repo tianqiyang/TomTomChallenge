5461a880b2a8142bf9aec4639f9e1115bbc35800
Tianqi Yang <tianqiy@berkeley.edu>
2020-05-25 12:01:44 -0700

    data example

--- home/views.py
+++ home/views.py
@@ -17,12 +17,12 @@ def default_map(request):
     args = {}
     #args = getAllData()
     #fake data
-    args['cook'] = {
-        'asian': [
+    args = {
+        'cook': [
+            'asian': [
             {
                 "coordinates": [-122.41157518699714, 37.78861143786813],
                 "food_name": "Pork Rice",
-                "description": "restaurant 1",
                 "Ingredients": "Fried pork, eggs,tomatoes, onions, brown rice",
                 "address": "865 Market St, San Francisco, CA 94103",
                 "food_image": "img/pork_rice.jpg",
@@ -30,10 +30,26 @@ def default_map(request):
                 "other_logos": ["img/1.png", "img/324.png", "img/323.png", ]
             }, {
                 "coordinates": [-122.43563039769336, 37.79623397692485],
-                "description": "restaurant 2"
+                "food_name": "Food 2",
+                "Ingredients": "Fried pork, eggs,tomatoes, onions, brown rice",
+                "address": "865 Market St, San Francisco, CA 94103",
+                "food_image": "img/pork_rice.jpg",
+                "restaurant_logo": "img/Panda_Express_logo.png",
+                "other_logos": ["img/1.png", "img/324.png", "img/323.png", ]
             }, {
                 "coordinates": [-122.45373134831641, 37.780423390391235],
-                "description": "restaurant 3"
-            }]
+                "food_name": "Food 3",
+                "Ingredients": "Fried pork, eggs,tomatoes, onions, brown rice",
+                "address": "865 Market St, San Francisco, CA 94103",
+                "food_image": "img/pork_rice.jpg",
+                "restaurant_logo": "img/Panda_Express_logo.png",
+                "other_logos": ["img/1.png", "img/324.png", "img/323.png", ]
+            }],
+        'fast_food': []
+        ],
+        'raw' :[
+            'fish': [],
+            'meat': []
+        ]
     }
     return HttpResponse(template.render(args, request))
